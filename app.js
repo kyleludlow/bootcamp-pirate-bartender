68ae5d76a2c18ef62992420945bc477440b816e2
Bhaumik Patel <bhaumik@thinkful.com>
2016-04-18 15:54:49 -0400

    Defines Questions object & prompts

--- app.js
+++ app.js
@@ -23,20 +23,55 @@ Bartender.prototype.createDrink = function(preferences) {
 };
 
 // create question object
+// var question1 = prompt('Do ye like yer drinks strong?');
+// var question2 = prompt('Do ye like it with a salty tang?');
+// var question3 = prompt('Are ye a lubber who likes it bitter?');
+// var question4 = prompt('Would ye like a bit of sweetness with yer poison?');
+// var question5 = prompt('Are ye one for a fruity finish?');
 
+var Question = function(questionText, flavor, like) {
+    this.questionText = questionText;
+    this.flavor = flavor;
+    this.like = false;
+    // method to capture & store into preferences() 
+    //console.log(userPrompt);
+};
 
+Question.prototype.pushPreferences = function() {
+    if (strongOrNo, saltyOrNo == "yes" || "Yes") {
+        // pushPreferences
+    } 
+    else if (strongOrNo == "no" || "No") {
+        // pushPreferences
+    }
+};
 
+var strongQuestion = new Question('Do ye like yer drinks strong?','strong');
+var saltyQuestion = new Question('Do ye like it with a salty tang?','salty');
+var bitterQuestion = new Question('Are ye a lubber who likes it bitter?', 'bitter');
+var sweetQuestion = new Question('Would ye like a bit of sweetness with yer poison?','sweet');
+var fruityQuestion = new Question('Are ye one for a fruity finish?','fruity');
 
+var strongOrNo = prompt(strongQuestion.questionText);
+var saltyOrNo = prompt(saltyQuestion.questionText);
+var bitterOrNo = prompt(bitterQuestion.questionText);
+var sweetOrNo = prompt(sweetQuestion.questionText);
+var fruityOrNo= prompt(fruityQuestion.questionText);
+
+/*
+
+// create ingredients 
 var ingredients = {
     Strong: ["salt", "butter"],
     Salty: [],
 }
 
+// create preferences 
 var preferences = function(this) {
 
     ////write if else statement to store preferences based on user prompts
     //pushes ingredients to pantry based on preference
-    this.preferenceStrong =  */
+    this.preferenceStrong =  
     this.preferenceSalty =
     this.preferenceSalty =
     this.preferenceSalty =
@@ -48,26 +83,10 @@ var preferences = function(this) {
         ]
 }
 
+//create pantry 
 var pantry = {};
 
+*/
 
-var question1 = prompt('Strong?');
-var question2 = prompt('Salty?');
-var question3 = prompt('Bitter?');
-var question4 = prompt('Sweet?');
-var question5 = prompt('Fruity?');
-
-
-question text
-preference value
-true or false
-method to capture & store into preferences()
-console.log(userPrompt);
-
-var question = function(questionText, flavor, like) {
-    this.questionText = questionText;
-    this.flavor = flavor;
-    this.like = false;
-};
 
 
