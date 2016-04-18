da50e7879546e47605f7baea2c8d1d9e514016e5
Kyle <kyleludlow@gmail.com>
2016-04-18 12:04:03 -0700

    creates user prompts, question objects

--- app.js
+++ app.js
@@ -7,42 +7,67 @@
  -pantry is an array  of all preferred ingredients
 
  -create a constructor function for preferences object
- */
+
+*/
 
 // create bartender object
 var Bartender = function() {
     // calls other methods
 }
 
-//adds createDrink to Bartender and takes parameter of Preference object
-Bartender.prototype.createDrink = function(preference) {
+//adds createDrink to Bartender that takes parameter of Preference object
+Bartender.prototype.createDrink = function(preferences) {
     console.log('Drink Created');
+    //using the preferences, create a drink using up to 5 random ingredients from the pantry object
+
+};
+
+// create question object
+
+
+
+
+var ingredients = {
+    Strong: ["salt", "butter"],
+    Salty: [],
+}
+
+var preferences = function(this) {
+
+    ////write if else statement to store preferences based on user prompts
+    //pushes ingredients to pantry based on preference
+    this.preferenceStrong =  */
+    this.preferenceSalty =
+    this.preferenceSalty =
+    this.preferenceSalty =
+    this.preferenceSalty =
+
+
+    if question1 = yes [
+
+        ]
+}
+
+var pantry = {};
+
+
+var question1 = prompt('Strong?');
+var question2 = prompt('Salty?');
+var question3 = prompt('Bitter?');
+var question4 = prompt('Sweet?');
+var question5 = prompt('Fruity?');
+
+
+question text
+preference value
+true or false
+method to capture & store into preferences()
+console.log(userPrompt);
+
+var question = function(questionText, flavor, like) {
+    this.questionText = questionText;
+    this.flavor = flavor;
+    this.like = false;
 };
 
 
-//prompt sequence
-
-var startGame = {
-    title: "Do ye like yer drinks strong?",
-        //text: "You will not be able to recover this imaginary file!",
-    type: "info",
-    showCancelButton: true,
-    confirmButtonColor: "#DD6B55",
-    confirmButtonText: "Yes, the stronger the better",
-    cancelButtonText: "Ew, no",
-    closeOnConfirm: false,
-    closeOnCancel: false
-}
-
-function startConditions(isConfirm) {
-    if (isConfirm) {
-        swal("Deleted!", "Your imaginary file has been deleted.", "success");
-    } else {
-        swal("Cancelled", "Your imaginary file is safe :)", "error");
-    }
-}
-
-
-
-//adds click prompt to button
-document.getElementById("promptButton").onclick = function() { swal(startGame, startConditions); };
